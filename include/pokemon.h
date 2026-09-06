30f56262aa5119109f28de93edb6e29a95664770
joaocardoso7 <joaovitor2008cardoso@gmail.com>
2026-09-05 22:31:22 -0300

    Define structures for Pokemon and Trainer representation

--- include/pokemon.h
+++ include/pokemon.h
@@ -1,1 +1,35 @@
+#ifndef POKEMON_H
+#define POKEMON_H
 
+#define MAX_ATAQUES 4
+#define MAX_TIME 6
+
+// Representação de uma habilidade/golpe
+typedef struct {
+    char nome[30];
+    int poder;
+    char tipo[15]; // "Fogo", "Agua", "Planta", "Normal"
+} Ataque;
+
+// Representação de uma criatura
+typedef struct {
+    char nome[30];
+    char tipo[15];
+    int vida_max;
+    int vida_atual;
+    int nivel;
+    int ataque;
+    int defesa;
+    Ataque ataques[MAX_ATAQUES]; // Vetor homogêneo de structs
+} Pokemon;
+
+// Representação do Jogador / Treinador
+typedef struct {
+    char nome[50];
+    Pokemon time[MAX_TIME];
+    int qtd_pokemon;
+    int pokebolas;
+    int pocoes;
+} Treinador;
+
+#endif
