5d54d8a1435f71d8d153e1f2c901f5037dc56f30
arthur navarete <arthurnavarete@gmail.com>
2026-09-18 15:57:32 -0300

    Add function to display Pokemon details

--- src/pokemon.c
+++ src/pokemon.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "../include/pokemon.h"
 
+
+void mostrar_pokemon(Pokemon *pokemon) {
+    printf("\n===== POKEMON =====\n");
+    printf("Nome: %s\n", pokemon->nome);
+    printf("Tipo: %s\n", pokemon->tipo);
+    printf("Nivel: %d\n", pokemon->nivel);
+    printf("Vida: %d/%d\n", pokemon->vida_atual, pokemon->vida_max);
+    printf("Ataque: %d\n", pokemon->ataque);
+    printf("Defesa: %d\n", pokemon->defesa);
+}
