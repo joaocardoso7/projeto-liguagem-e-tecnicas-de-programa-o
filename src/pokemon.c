17dc55e51ab49cf1ef247250d16d09d6990a55ab
arthur navarete <arthurnavarete@gmail.com>
2026-09-18 16:01:00 -0300

    Add inventory and trainer initialization functions

--- src/pokemon.c
+++ src/pokemon.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include <string.h>
 #include "../include/pokemon.h"
-
 
 void mostrar_pokemon(Pokemon *pokemon) {
     printf("\n===== POKEMON =====\n");
@@ -11,4 +9,18 @@ void mostrar_pokemon(Pokemon *pokemon) {
     printf("Vida: %d/%d\n", pokemon->vida_atual, pokemon->vida_max);
     printf("Ataque: %d\n", pokemon->ataque);
     printf("Defesa: %d\n", pokemon->defesa);
+}
+
+void mostrar_inventario(Treinador *treinador) {
+    printf("\n===== INVENTARIO =====\n");
+    printf("Pokebolas: %d\n", treinador->pokebolas);
+    printf("Pocoes: %d\n", treinador->pocoes);
+}
+
+void inicializar_treinador(Treinador *treinador, const char *nome) {
+    snprintf(treinador->nome, sizeof(treinador->nome), "%s", nome);
+
+    treinador->qtd_pokemon = 0;
+    treinador->pokebolas = 5;
+    treinador->pocoes = 3;
 }
