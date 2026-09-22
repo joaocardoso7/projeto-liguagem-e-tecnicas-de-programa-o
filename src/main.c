30f9774d44ca7ed07a2de32e2692425169db06f6
joaocardoso7 <joaovitor2008cardoso@gmail.com>
2026-09-22 17:22:52 -0300

    Implement basic game menu in main.c
    Add initial menu options for the RPG Pokémon game.

--- src/main.c
+++ src/main.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(void) {
+    printf("=== RPG POKEMON ===\n");
+    printf("1. Iniciar Jogo\n");
+    printf("2. Sair\n");
+    
+    return 0;
+}
